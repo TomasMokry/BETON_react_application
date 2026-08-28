f2225ce23dbf895d31a22ec3eb9df5bde487cd4d
Tomas Mokry <tomas.mokry@gmail.com>
2026-08-28 16:53:10 +0200

    grid fix

--- src/layouts/SearchProductPage/ProductPage.tsx
+++ src/layouts/SearchProductPage/ProductPage.tsx
@@ -264,7 +264,7 @@ export const ProductPage = () => {
           {isLoadingProducts ? (
             <div>Loading products...</div>
           ) : (
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-3 mx-1">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4 g-3 mx-1">
               {products.map((product) => (
                 <Product
                   product={product}
